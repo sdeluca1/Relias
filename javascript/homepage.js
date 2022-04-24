d3d588ef895606938a1de208106a4d6fb05af36e
sdeluca1 <sdeluca51@yahoo.com>
2022-04-23 23:52:31 -0400

    Added Trending button and function

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -4,8 +4,15 @@ async function movieSearch(keyword) {
     return json;
 }
 
-$(".searchButton").on('click', async function() {
-    $(".results").empty()
+
+
+
+
+
+
+
+$(".searchButton").on('click', async function() { //Main function: Movie Title Search
+    $(".titleResults").empty()
       let searchQuery = $(".searchBar").val()
         if (searchQuery == "") {
             alert("I'm sorry, your search did not match any movies in our records. :( Try again to find more movies!")
@@ -13,17 +20,41 @@ $(".searchButton").on('click', async function() {
       let searchResult = await movieSearch(searchQuery)
       if (searchResult.results.length == 0) {
         alert("I'm sorry, your search did not match any movies in our records. :( Try again to find more movies!")
-        $(".results").append("<p> No movies found containing " + searchQuery + "." + "</p>")
+        $(".titleResults").append("<p> No movies found containing " + searchQuery + "." + "</p>")
      }
       console.log(searchResult)
      searchResult.results.forEach(i => {
-         $(".results").append("<li>"+ i.original_title + ' <button class = "overview">Overview </button>' + "</li>")
-         $(".overview").on('click', async function() {
-             //$(".results").append("<p>" + i.overview + "</p")
-             alert("Overview of " + i.original_title + " :" + i.overview)
-         });
+         $(".titleResults").append("<li>"+ i.original_title  + "</li>")
+     
      });
+  
     }
+ 
+})
+
+/*$(".overview").on('click', async function() {
+    searchResult.results.forEach(j=> {
+ $(".results").append("<p>" + j.overview + "</p")
+})
+
+    alert("Overview of " + i.original_title + ": " + i.overview)
+});
+*/
+
+async function getTrending(){ //Gets the top trending movies in the database.
+    let tmovies = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=46ae1d2187fb631facdf70783362f90a")
+    let jsont = await tmovies.json()
+    return jsont;
+}
+
+$(".trendingButton").on('click', async function() {
+    $(".trendingResults").empty()
+    $(".trendingResults").append("<p>" + "Trending Movies:" + "</p>")
+      let trendingResult = await getTrending()
+      console.log(trendingResult)
+      trendingResult.results.forEach(i => {
+          $(".trendingResults").append("<li>" + i.title + "</li>")
+      })  
 })
 
 
